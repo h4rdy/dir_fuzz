260e733fbb6c49c7779647aa0fd5d15d17d20bac
h4rdy <with.h4rdy@gmail.com>
2016-07-16 17:48:56 +0800

    fix bug

--- dir_fuzz.py
+++ dir_fuzz.py
@@ -15,7 +15,7 @@ dirqueue = Queue.Queue()
 mutex = threading.Lock()
 headers = {
 		  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20',
-	      'Referer' : 'http://www.baidu.com'
+	      'Referer' : 'http://www.baidu.com',
           'Mark': 'dirfuzz'
           }
 cookies = {} #自定义cookies
